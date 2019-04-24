aaa22a936ab2874baa9583c6ad40021780535af6
BabySchwal <43013276+BabySchwal@users.noreply.github.com>
2019-04-23 22:54:05 -0600

    Update script.js

--- style/script.js
+++ style/script.js
@@ -1,5 +1,4 @@
 //This file is for the calendar
-
 window.onload = function()
 {
 	var d = new Date();
@@ -10,16 +9,18 @@ window.onload = function()
 	var tmp = new Date(first_date).toDateString();
 	var first_day = tmp.substring(0, 3); //mon
 	var day_name = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
-	var day_no = day_name.indexOf(first_date);
+	var day_no = day_name.indexOf(first_day);
 	var days = new Date(year, month+1, 0).getDate(); //total days in current month
-	var calendar = get_calendar(day_no, days);
+	var calendar = get_calendar(day_no, days, d);
 	document.getElementById("calendar-month-year").innerHTML = month_name[month]+" "+year;
 	document.getElementById("calendar-dates").appendChild(calendar);
 }
 
-function get_calendar(day_no, days)
+function get_calendar(day_no, days, d)
 {
 	var table = document.createElement('table');
+	table.setAttribute('id', 'cal_table');
+	table.setAttribute('name', d);
 	var tr = document.createElement('tr');
 
 	for(var c=0; c<=6; c++)
@@ -47,7 +48,9 @@ function get_calendar(day_no, days)
 	for(; c<=6; c++)
 	{
 		var td = document.createElement('td');
+		td.setAttribute('onclick', 'cell_date()');
 		td.innerHTML = count;
+		count++;
 		tr.appendChild(td);
 	}
 	table.appendChild(tr);
@@ -63,10 +66,58 @@ function get_calendar(day_no, days)
 				return table;
 			}
 			var td = document.createElement('td');
+			td.setAttribute('onclick', 'cell_date()');
 			td.innerHTML = count;
 			count++;
 			tr.appendChild(td);
 		}
 		table.appendChild(tr);
 	}
+}
+
+cell_date = function()
+{
+  	confirm("No assignments are due on this date");
+}
+
+prev_month = function()
+{
+	var calendar = document.getElementById("cal_table");
+	var dstr = calendar.getAttribute("name");
+	var d = new Date(dstr);
+	document.getElementById("calendar-dates").removeChild(calendar);
+	d.setMonth(d.getMonth()-1);
+	var month_name = ['January','Februrary','March','April','May','June','July','August','Sepetmeber','October','Novemeber','Decemeber'];		
+	var month = d.getMonth(); //current month
+	var year = d.getFullYear(); //current year
+	var first_date = month_name[month] + " " + 1 + " " + year;
+	var tmp = new Date(first_date).toDateString();
+	var first_day = tmp.substring(0, 3); //mon
+	var day_name = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
+	var day_no = day_name.indexOf(first_day);
+	var days = new Date(year, month+1, 0).getDate(); //total days in current month
+	calendar = get_calendar(day_no, days, d);
+	document.getElementById("calendar-month-year").innerHTML = month_name[month]+" "+year;
+	document.getElementById("calendar-dates").appendChild(calendar);
+}
+
+next_month = function()
+{
+	var calendar = document.getElementById("cal_table");
+	var dstr = calendar.getAttribute("name");
+	var d = new Date(dstr);
+	document.getElementById("calendar-dates").removeChild(calendar);
+	d.setMonth(d.getMonth()+1);
+	var month_name = ['January','Februrary','March','April','May','June','July','August','Sepetmeber','October','Novemeber','Decemeber'];		
+	var month = d.getMonth(); //current month
+	var year = d.getFullYear(); //current year
+	var first_date = month_name[month] + " " + 1 + " " + year;
+	var tmp = new Date(first_date).toDateString();
+	var first_day = tmp.substring(0, 3); //mon
+	var day_name = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
+	var day_no = day_name.indexOf(first_day);
+	var days = new Date(year, month+1, 0).getDate(); //total days in current month
+	calendar = get_calendar(day_no, days, d);
+	document.getElementById("calendar-month-year").innerHTML = month_name[month]+" "+year;
+	document.getElementById("calendar-dates").appendChild(calendar);
 }
